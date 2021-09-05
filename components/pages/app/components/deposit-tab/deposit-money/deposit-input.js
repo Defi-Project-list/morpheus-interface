2f7ae7f6501eea677f5fae3c4a19502db5529672
ss0on <achiaga.10@gmail.com>
2021-09-05 20:39:35 +0200

    feat: withdraw working

--- components/pages/app/components/deposit-tab/deposit-money/deposit-input.js
+++ components/pages/app/components/deposit-tab/deposit-money/deposit-input.js
@@ -1,69 +1,67 @@
-import NumberFormat from 'react-number-format'
+import NumberFormat from 'react-number-format';
 
-const Button = ({ onClick, children }) => {
-	return (
-		<button
-			onClick={onClick}
-			className='w-full uppercase text-lg font-semibold px-2 py-3 rounded-lg bg-primary-500 text-white hover:-border-2 hover:bg-primary-600  hover:shadow-lg'>
-			{children}
-		</button>
-	)
-}
+export const Button = ({ onClick, children }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="w-full uppercase text-lg font-semibold px-2 py-3 rounded-lg bg-primary-500 text-white hover:-border-2 hover:bg-primary-600  hover:shadow-lg"
+    >
+      {children}
+    </button>
+  );
+};
 
 const DepositInput = ({
-	screenState,
-	acceptedAllowance,
-	deposit,
-	withdraw,
-	submitDeposit,
-	submitWithdraw,
-	submitApprove,
-	setDeposit,
-	setWithdraw
+  screenState,
+  acceptedAllowance,
+  deposit,
+  withdraw,
+  submitDeposit,
+  submitWithdraw,
+  submitApprove,
+  setDeposit,
+  setWithdraw,
 }) => {
-	const handleDeposit = (values) => {
-		const { value } = values
-		setDeposit(value)
-	}
+  const handleDeposit = (values) => {
+    const { value } = values;
+    setDeposit(value);
+  };
 
-	const handleWithdraw = (e) => {
-		setWithdraw(e.target.value)
-	}
+  return (
+    <div className="w-full flex justify-center items-center">
+      <div className="mt-12 w-10/12 flex flex-col items-center justify-center py-4 h-40">
+        <div className="w-3/5">
+          <p className="w-full text-left text-lg mb-2">Deposit Amount</p>
+          <div className="flex mb-8">
+            <NumberFormat
+              value={deposit}
+              onValueChange={handleDeposit}
+              thousandSeparator={true}
+              inputMode="numeric"
+              className="w-full border-t border-l border-b border-gray-200 pt-2 pb-2 rounded-t-lg rounded-l-lg rounded-b-lg rounded-r-none text-2xl text-right pr-4"
+            />
+            <div
+              className="py-1 px-3 rounded-t-lg rounded-r-lg rounded-b-lg rounded-l-none text-xl bg-primary-100 flex items-center"
+              style={{ width: '100px' }}
+            >
+              <img
+                src="https://raw.githubusercontent.com/uniswap/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png"
+                width="20px"
+              />
+              <span className="ml-1">DAI</span>
+            </div>
+          </div>
+          <div className="w-full flex justify-center items-center">
+            {acceptedAllowance ? (
+              <Button onClick={submitDeposit}>Deposit Amount</Button>
+            ) : (
+              <Button onClick={submitApprove}> Approve Transfer</Button>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-	return (
-		<div className='w-full flex justify-center items-center'>
-			<div className='mt-12 w-10/12 flex flex-col items-center justify-center py-4 h-40'>
-				<div className='w-3/5'>
-					<p className='w-full text-left text-lg mb-2'>Deposit Amount</p>
-					<div className='flex mb-8'>
-						<NumberFormat
-							value={deposit}
-							onValueChange={handleDeposit}
-							thousandSeparator={true}
-							inputMode='numeric'
-							className='w-full border-t border-l border-b border-gray-200 pt-2 pb-2 rounded-t-lg rounded-l-lg rounded-b-lg rounded-r-none text-2xl text-right pr-4'
-						/>
-						<div
-							className='py-1 px-3 rounded-t-lg rounded-r-lg rounded-b-lg rounded-l-none text-xl bg-primary-100 flex items-center'
-							style={{ width: '100px' }}>
-							<img
-								src='https://raw.githubusercontent.com/uniswap/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png'
-								width='20px'
-							/>
-							<span className='ml-1'>DAI</span>
-						</div>
-					</div>
-					<div className='w-full flex justify-center items-center'>
-						{acceptedAllowance ? (
-							<Button onClick={submitDeposit}>Deposit Amount</Button>
-						) : (
-							<Button onClick={submitApprove}> Approve Transfer</Button>
-						)}
-					</div>
-				</div>
-			</div>
-		</div>
-	)
-}
-
-export default DepositInput
+export default DepositInput;
--- components/pages/app/components/withdraw-tab/withdraw-money/index.js
+++ components/pages/app/components/withdraw-tab/withdraw-money/index.js
@@ -1,5 +1,47 @@
+import NumberFormat from 'react-number-format';
+import { Button } from '../../deposit-tab/deposit-money/deposit-input';
+import { convertToBigNum } from '../../../utils/convertBigNumber';
+import { useContext, useState } from 'react';
+import axios from 'axios';
+import { contractAddress } from '@components/pages/app/config/addresses';
+import { WalletContext } from '@components/pages/app';
+
+const fetchGas = () => axios.get(contractAddress.GAS_STATION);
+
 const WithdrawMoney = () => {
-	return <div>WithdrawMoney</div>;
+  const { contracts } = useContext(WalletContext);
+  const [withdraw, setWithdraw] = useState(0);
+  const handleWithdraw = (values) => {
+    const { value } = values;
+    setWithdraw(value);
+  };
+  const withdrawAmount = async () => {
+    const gasPrice = await fetchGas().then((r) => r.data?.standard * 1e9);
+    const withdrawAmount = convertToBigNum(withdraw);
+    try {
+      const tx = await contracts.vault.withdraw(withdrawAmount, {
+        gasPrice,
+      });
+      const res = await tx.wait();
+      console.log({ res });
+    } catch (err) {
+      console.error(err, 'error deposit');
+    }
+  };
+  return (
+    <div>
+      <NumberFormat
+        value={withdraw}
+        onValueChange={handleWithdraw}
+        thousandSeparator={true}
+        inputMode="numeric"
+        className="w-full border-t border-l border-b border-gray-200 pt-2 pb-2 rounded-t-lg rounded-l-lg rounded-b-lg rounded-r-none text-2xl text-right pr-4"
+      />
+      <div className="w-full flex justify-center items-center">
+        <Button onClick={withdrawAmount}>Withdraw Amount</Button>
+      </div>
+    </div>
+  );
 };
 
 export default WithdrawMoney;
