cde99fd44a812080e4478bdcb7a83430a5bd43f7
ss0on <achiaga.10@gmail.com>
2021-09-05 11:58:08 +0200

    feat: imporovements to wallet modal and network validation

--- components/pages/app/components/app-header/address-header.js
+++ components/pages/app/components/app-header/address-header.js
@@ -2,25 +2,47 @@ import RobotAddress from '@assets/icons/robot-address'
 
 import { useEthers } from '../../config'
 
-const AddressHeader = () => {
-	const { providerAddress, openModal } = useEthers()
+const ConnectWalletButton = ({ openModal, error }) => {
+	if (error) {
+		return (
+			<button
+				onClick={openModal}
+				className={`first-letter:capitalize w-min rounded-lg py-1 px-4 bg-red-500 text-white whitespace-nowrap 	hover:bg-red-600`}>
+				{error}
+			</button>
+		)
+	}
+	return (
+		<button
+			onClick={openModal}
+			className={`
+		 	border
+			border-transparent
+			first-letter:capitalize
+			w-min rounded-md py-1 px-4 
+			bg-accent-200 	
+			text-accent-500 
+			whitespace-nowrap
+			hover:border-accent-500
+			 `}>
+			Connect Wallet
+		</button>
+	)
+}
 
+function formatWalletAddress(walletAddress) {
+	return `${walletAddress?.slice(0, 10)}...
+					${walletAddress?.slice(walletAddress?.length - 4, walletAddress?.length)}`
+}
+
+const AddressHeader = () => {
+	const { providerAddress, openModal, error } = useEthers()
 	return (
 		<div className='flex justify-end w-1/3 items-end text-primary-500 font-semibold items-center'>
 			{providerAddress ? (
-				<p className='mr-6 text-lg'>
-					{providerAddress?.slice(0, 10)}...
-					{providerAddress?.slice(
-						providerAddress?.length - 4,
-						providerAddress?.length
-					)}
-				</p>
+				<p className='mr-6 text-lg'>{formatWalletAddress(providerAddress)}</p>
 			) : (
-				<button
-					onClick={openModal}
-					className={`first-letter:capitalize w-min rounded-lg py-1 px-4 bg-accent-500 text-white`}>
-					Connect
-				</button>
+				<ConnectWalletButton openModal={openModal} error={error} />
 			)}
 			<RobotAddress w='60px' h='60px' />
 		</div>
--- components/pages/app/config/index.js
+++ components/pages/app/config/index.js
@@ -11,6 +11,8 @@ import { convertToNum } from '../utils/convertBigNumber'
 
 const INFURA_ID = '460f40a260564ac4a4f4b3fffb032dad'
 
+const WRONG_NETWORK = 'wrong network'
+
 const providerOptions = {
 	walletconnect: {
 		package: WalletConnectProvider, // required
@@ -45,18 +47,19 @@ const providerOptions = {
 export const useEthers = () => {
 	const [wallet, setWallet] = useState({})
 	const [approvalEvents, setApprovalEvents] = useState()
+	const [error, setError] = useState()
 	const [vaultContract, setVaultContract] = useState()
 	const [tokenContract, setTokenContract] = useState()
 
 	async function getWalletInfo(provider) {
 		const web3Provider = new providers.Web3Provider(provider)
-		const signer = await web3Provider.getSigner()
-		const address = await signer.getAddress()
 		const network = await web3Provider.getNetwork()
 		if (network?.name !== 'matic') {
-			alert('Incompatibale network')
-			return {}
+			setError(WRONG_NETWORK)
+			return null
 		}
+		const signer = await web3Provider.getSigner()
+		const address = await signer.getAddress()
 		const balance = await web3Provider.getBalance(address)
 		setWallet({ address, balance })
 		return signer
@@ -77,6 +80,7 @@ export const useEthers = () => {
 	}
 
 	function getContracts(signer) {
+		console.log({ signer })
 		if (!signer) return null
 		const _vault_contract = new ethers.Contract(
 			contractAddress.DAI_SAVERS_VAULT,
@@ -95,6 +99,7 @@ export const useEthers = () => {
 	}
 
 	function listenProviderEvents(provider) {
+		if (!provider) return
 		provider.on('accountsChanged', (accounts) => {
 			getWalletInfo(provider)
 			console.log('accountsChanged', accounts)
@@ -106,14 +111,24 @@ export const useEthers = () => {
 		})
 
 		// Subscribe to networkId change
-		provider.on('networkChanged', (networkId) => {
-			console.log('networkChanged', networkId)
+		provider.on('networkChanged', async (networkId) => {
+			const web3Provider = new providers.Web3Provider(provider)
+			const network = await web3Provider.getNetwork()
+			if (network?.name !== 'matic') {
+				setError(WRONG_NETWORK)
+			} else {
+				setError(null)
+			}
 		})
 	}
 
 	async function init() {
 		try {
-			const { provider, signer } = await getProvider()
+			const { provider, signer, error } = await getProvider()
+			if (error) {
+				setError(error)
+				return null
+			}
 			await getContracts(signer)
 			listenProviderEvents(provider)
 		} catch (e) {
@@ -126,26 +141,11 @@ export const useEthers = () => {
 		init()
 	}
 
-	// const _initializeEthers = async () => {
-	// 	// BEST PRACTICE PROVIDER
-	// 	// The "any" network will allow spontaneous network changes
-	// 	const _provider = new ethers.providers.Web3Provider(window.ethereum, 'any')
-	// 	_provider.on('network', (newNetwork, oldNetwork) => {
-	// 		// When a Provider makes its initial connection, it emits a "network"
-	// 		// event with a null oldNetwork along with the newNetwork. So, if the
-	// 		// oldNetwork exists, it represents a changing network
-	// 		if (oldNetwork) {
-	// 			window.location.reload()
-	// 		}
-	// 	})
-
-	// 	// const _approvalEvents = _token_contract.filters.Approval(null, userAddress);
-
-	// }
 	return {
 		providerAddress: wallet?.address,
 		tokenContract,
 		vaultContract,
-		openModal
+		openModal,
+		error
 	}
 }
